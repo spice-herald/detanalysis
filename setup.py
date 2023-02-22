478bddeb99ea0f57490a2b05064ec9ac7347004d
Bruno Serfass <serfass@oak.berkeley.edu>
2023-02-21 16:37:30 -0800

    new release

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class CleanCommand(Command):
 
 setup(
     name="detanalysis",
-    version="0.1.0",
+    version="0.2.0",
     description="Data Analysis Package",
     long_description=long_description,
     long_description_content_type='text/markdown',
