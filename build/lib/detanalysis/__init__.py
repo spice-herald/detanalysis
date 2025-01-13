15dab0d3e08ab7ec89eeeb6f9cc4c59dba29938c
wmatava <wmatava@berkeley.edu>
2025-01-13 11:28:54 -0800

    Removing local pip build files

--- build/lib/detanalysis/__init__.py
+++
@@ -1,5 +0,0 @@
-from .analyzer import Analyzer
-from .semiautocuts import Semiautocut, MasterSemiautocuts
-from .scatterplotter import ScatterPlotter
-from .photon_calibration import PhotonCalibration
-from .func import *
