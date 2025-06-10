9d47aa4e5f024a030e4ae9636ad87797e49d23ee
Maggie Reed <106180791+magsreed@users.noreply.github.com>
2025-06-10 15:47:19 -0700

    Updated demo for clarity

---
+++ build/lib/detanalysis/__init__.py
@@ -0,0 +1,5 @@
+from .analyzer import Analyzer
+from .semiautocuts import Semiautocut, MasterSemiautocuts
+from .scatterplotter import ScatterPlotter
+from .photon_calibration import PhotonCalibration
+from .func import *
